9bce4f65d1095365b6bd4aa125104b8cd74da5a0
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2017-06-04 22:07:14 +0200

    And a cool notebook

--- train.py
+++ train.py
@@ -1,18 +1,12 @@
 # TODO Logging and Tests are essential!
 
 import sys
-import logging
-import random
-from typing import Tuple, Iterator
 
 import numpy as np
 import keras
 
 import vixstructure.models as models
 import vixstructure.data as data
-
-
-
 
 
 def get_model(hidden_layers, past_days, days_to_future, step_size=0.01):
@@ -22,74 +16,14 @@ def get_model(hidden_layers, past_days, days_to_future, step_size=0.01):
     return model
 
 
-def data_generator(data: np.ndarray, past_days: int, days_to_future: int,
-                   shuffle: bool=False
-                   ) -> Iterator[np.ndarray]:
-    # TODO Specify batch size.
-    index_range = list(range(past_days, len(data) - days_to_future))
-    while True:  # Generator should loop indefinitely.
-        if shuffle:
-            random.shuffle(index_range)
-        for i in index_range:
-            # Expand dimension --> batch size of 1
-            yield (np.expand_dims(data[i-past_days:i], axis=0),
-                   np.expand_dims(data[i+days_to_future,1:], axis=0))
-
-
-def get_data_generators(past_days: int, days_to_future: int,
-             split: float=0.80, min_index: int=None, max_index: int=None,
-             ) -> Tuple[
-                    Tuple[int, Iterator[np.ndarray]],
-                    Tuple[int, Iterator[np.ndarray]]]:
-    """
-    Get two generators, both which loop over their data indefinitely. The first one
-    is for training, the second one for validation. Also returns the number of
-    unique data samples until the generator starts the next loop.
-    :param past_days:
-    :param days_to_future:
-    :param split: Fraction at which to split the data into training and test set.
-    :param min_index: If you don't want to use the whole data (maybe because you also
-                      need a test set) you can specify a range with ``min_index`` and
-                      ``max_index``. Is ignored when greater than the data length.
-    :param max_index: See ``min_index``.
-    :return: A tuple of two tuples:
-             1. tuple: (number of unique training samples, training data generator)
-             2. tuple: (number of unique validation samples, validation data generator)
-    """
-    assert 0. < split < 1.
-    if min_index: assert min_index >= 0
-    if max_index: assert min_index < max_index
-    training = data.get_data(normalized=True)
-    # Check indixes.
-    if min_index and min_index >= len(training):
-        logging.warning(f"min_index is greater than length of data {len(training)}. Ignore.")
-        min_index = None
-    if max_index and max_index >= len(training):
-        logging.warning(f"max_index is greater than length of data {len(training)}. Ignore.")
-        max_index = None
-    # Fill the NaN values and extract a numpy array.
-    training = training.fillna(0).values[min_index:max_index]
-    split_index = int(split * len(training))
-    # Split data into validation set and training set.
-    validation = training[split_index:]
-    nr_samples_validation = len(validation) - past_days - days_to_future
-    assert nr_samples_validation > 0
-    training = training[:split_index]
-    nr_samples_training = len(training) - past_days - days_to_future
-    assert nr_samples_training > 0
-    return ((nr_samples_training, data_generator(training, past_days, days_to_future, shuffle=True)),
-            (nr_samples_validation, data_generator(validation, past_days, days_to_future)))
-
-
 def train(hidden_layers, past_days, days_to_future, epochs=100, verbose=1):
     repr_string = f"{hidden_layers}_{past_days}_{days_to_future}"
-    (testlen, testgen), (vallen, valgen) = get_data_generators(past_days, days_to_future)
+    (testlen, testgen), (vallen, valgen) = data.get_data_generators(past_days, days_to_future)
     model = get_model(hidden_layers, past_days, days_to_future)
     history = model.fit_generator(testgen, testlen, epochs=epochs, verbose=verbose,
                                   validation_data=valgen, validation_steps=vallen,
                                   callbacks=[keras.callbacks.CSVLogger(f"./logs/naive-fully-connected/training_{repr_string}.log"),
                                              keras.callbacks.TensorBoard("./logs/tensorboard")])
-                                             #keras.callbacks.EarlyStopping(min_delta=1e-5, patience=5, verbose=1)])
     model.save(f"./models/naive-fully-connected/naive_{repr_string}.hdf5")
     return model, history
 
