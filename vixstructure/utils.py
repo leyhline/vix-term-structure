4d7903de3fe2e246d758a2ad35b4a890329755a5
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2017-07-10 22:55:45 +0200

    Notebook for correct comparison of all experiments up till now

--- vixstructure/utils.py
+++ vixstructure/utils.py
@@ -7,7 +7,11 @@ Some helper functions for evaluating the data.
 """
 
 import datetime
+import os
 from collections import namedtuple
+
+import pandas as pd
+import numpy as np
 
 
 ModelParameters = namedtuple("ModelParameters", ("datetime", "hostname", "depth", "width",
@@ -32,3 +36,25 @@ def parse_model_repr(repr_str: str):
     repr_dict["normalized"] = True if len(repr_list) >= 8 and repr_list[7] == "normalized" else False
     return ModelParameters(**repr_dict)
 
+
+def parse_whole_directory(path: str):
+    directory, _, files = next(os.walk(path))
+    csv_files = tuple(file for file in files if os.path.splitext(file)[1] == ".csv")
+    stats = [(parse_model_repr(os.path.splitext(file)[0]),
+              pd.read_csv(os.path.join(directory, file), header=0, index_col=0,
+                          dtype={"epoch": int, "loss": np.float32, "val_loss": np.float32,
+                                 "denorm_mse": np.float32, "val_denorm_mse": np.float32}))
+             for file in csv_files]
+    parameters, stats_data = zip(*stats)
+    for d in stats_data:
+        if len(d.columns) == 2:
+            use_this_as_columns = d.columns
+            break
+    for d in stats_data:
+        if len(d.columns) == 4:
+            d.drop(["loss", "val_loss"], axis=1, inplace=True)
+            d.columns = use_this_as_columns
+    dataframe = pd.concat(stats_data, keys=[(p.depth, p.width, p.normalized, p.datetime) for p in parameters],
+                          names=["depth", "width", "normalized", "datetime"])
+    dataframe.sort_index(inplace=True)
+    return dataframe
