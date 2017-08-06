3a99c5172267ef02781b8c6b692c6696b12f85d8
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2017-08-06 22:15:56 +0200

    Add setup file for easy installation via pip

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='vixstructure',
+    version='0.3',
+    description=('Exploring the VIX term structure with Machine Learning '
+                  'for my Bachelor\'s thesis.'),
+    url='https://github.com/leyhline/vix-term-structure',
+    author='Thomas Leyh',
+    author_email='leyht@informatik.uni-freiburg.de',
+    license='MIT',
+    keywords='deeplearning machinelearning finance',
+    packages=['vixstructure'],
+    install_requires=['tensorflow',
+                      'pandas',
+                      'numpy',
+                      'jupyter',
+                      'matplotlib',
+                      'h5py',
+                      'lazy',
+                      'tables'],
+    python_requires='>=3.5',
+)
