c2a8126addfc0ceb41678c7b17e41d34dca42ebb
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2017-08-10 22:23:52 +0200

    Created train2.py for training with second data representation.

--- train2.py
+++ train2.py
@@ -1,5 +1,6 @@
 """
 CLI for training feedforward neural networks with prepared data.
+Second data representation after first approach failed.
 """
 
 import argparse
@@ -22,53 +23,22 @@ parser.add_argument("-op", "--optimizer", choices=["Adam", "SGD"], default="Adam
 parser.add_argument("-lr", "--learning_rate", type=float, default=0.001)
 parser.add_argument("-bs", "--batch_size", type=int, default=32)
 parser.add_argument("-e", "--epochs", type=int, default=100)
-parser.add_argument("-n", "--normalize", action="store_true")
 parser.add_argument("-q", "--quiet", action="store_true")
 parser.add_argument("-s", "--save", type=str, help="Optional: Specify save path for trained model.")
-parser.add_argument("-m", "--model", choices=["term_structure_to_spread_price",
-                                              "term_structure_to_spread_price_v2"],
-                    default="term_structure_to_spread_price",
-                    help="See models defined in vixstructure.models.")
 parser.add_argument("-a", "--activation", default="relu", help="Activation function for hidden layers.",
                     choices=["relu", "selu"])
-parser.add_argument("--data", choices=["LongPricesDataset", "VIXLongPrice", "MinutelyData"],
-                    default="LongPricesDataset",
-                    help="Which data to use for training and validation.")
-parser.add_argument("--days", type=int, default=1, help="How many days to predict into the future.")
 parser.add_argument("--reduce_lr", action="store_true", help="If validation loss stagnates, reduce lr by sqrt(0.1).")
 parser.add_argument("--shuffle_off", action="store_false", help="Don't shuffle training data.")
-parser.add_argument("--include_months", action="store_true")
-parser.add_argument("--include_days", action="store_true")
 
 
 def train(args):
-    if args.data == "LongPricesDataset":
-        input_length = 9
-        input_length += int(args.include_months)
-        input_length += int(args.include_days)
-    elif args.data == "VIXLongPrice":
-        input_length = 8
-    else:
-        input_length = 9
-    model = getattr(models, args.model)(args.network_depth, args.network_width, args.dropout,
-                                        activation_function=args.activation, input_data_length=input_length)
+    input_data_length = 8
+    model = models.term_structure_to_single_spread_price(args.network_depth, args.network_width,
+                                                         args.dropout, input_data_length, args.activation)
     optimizer = getattr(keras.optimizers, args.optimizer)(lr=args.learning_rate)
-    if args.data == "LongPricesDataset":
-        dataset = getattr(data, args.data)("data/8_m_settle.csv", "data/expirations.csv")
-        (x_train, y_train), (x_val, y_val), _ = dataset.splitted_dataset(normalize=args.normalize,
-                                                                         with_months=args.include_months,
-                                                                         with_days=args.include_days,
-                                                                         days_to_future=args.days)
-    elif args.data == "MinutelyData":
-        dataset = getattr(data, args.data)("data/term_structure_minutely.h5")
-        (x_train, y_train), (x_val, y_val), _ = dataset.splitted_dataset()
-    else:
-        dataset = getattr(data, args.data)("data/8_m_settle.csv", "data/expirations.csv", "data/vix.csv")
-        (x_train, y_train), (x_val, y_val), _ = dataset.splitted_dataset(with_expirations=True, with_vix=True)
-    metrics = []
-    if args.normalize:
-        metrics.append(dataset.denorm_mse)
-    model.compile(optimizer, "mean_squared_error", metrics=metrics)
+    dataset = data.FutureswiseLongPrice("data/futureswise_mapping.h5")
+    (x_train, y_train), (x_val, y_val), _ = dataset.splitted_dataset()
+    model.compile(optimizer, "mean_squared_error")
     callbacks = []
     if args.save:
         now = datetime.datetime.now()
@@ -77,11 +47,9 @@ def train(args):
             socket.gethostname(),
             args.network_depth,
             args.network_width,
-            args.days,
             0 if not args.dropout else args.dropout,
             args.optimizer,
-            args.learning_rate,
-            "_normalized" if args.normalize else "")
+            args.learning_rate)
         callbacks.append(keras.callbacks.CSVLogger(os.path.join(args.save, name + ".csv")))
     if args.reduce_lr:
         callbacks.append(keras.callbacks.ReduceLROnPlateau(factor=sqrt(0.1), patience=100, verbose=1))
