17a97f3160fe32278dab5b5613b6d11d526209f8
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2017-08-16 14:43:53 +0200

    utility function for evaluation of new data representation

--- tests/test_vixstructure/test_utils.py
+++ tests/test_vixstructure/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from vixstructure.utils import parse_model_repr
+from vixstructure.utils import parse_model_repr, parse_model_repr_old, parse_model_repr_monthwise
 
 
 class TestUtils(unittest.TestCase):
@@ -11,6 +11,18 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(len(result2), 9)
         self.assertEqual(result1.normalized, False)
         self.assertEqual(result2.normalized, True)
+        
+    def test_parse_model_repr_old(self):
+        result1 = parse_model_repr_old("20170703100041_tfpool08_depth9_width18_dropout0e+00_optimAdam_lr1e-03")
+        result2 = parse_model_repr_old("20170703100621_tfpool17_depth9_width21_dropout0e+00_optimAdam_lr1e-03_normalized")
+        self.assertEqual(len(result1), 8)
+        self.assertEqual(len(result2), 8)
+        self.assertEqual(result1.normalized, False)
+        self.assertEqual(result2.normalized, True)
+        
+    def test_parse_model_repr_monthwise(self):
+        result1 = parse_model_repr_monthwise("20170816135917_tfpool45_depth9_width27_month10_dropout0e+00_optimAdam_lr1e-03")
+        self.assertEqual(len(result1), 8)
 
 
 if __name__ == '__main__':
